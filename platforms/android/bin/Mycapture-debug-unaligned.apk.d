36f86328fe77ede7f4a1568e17ba503a8f93e627
diki <8193481952qq.com>
2014-05-19 11:01:55 +0800

    “提交注释”

---
+++ platforms/android/bin/Mycapture-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\capture\platforms\android\bin\Mycapture-debug-unaligned.apk : \
+C:\capture\platforms\android\bin\Mycapture.ap_ \
+C:\capture\platforms\android\bin\classes.dex \
+C:\capture\platforms\android\CordovaLib\bin\classes.jar \
